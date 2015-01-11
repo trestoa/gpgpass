de39f83fa598cdd7625296679dae233989a9113d
Markus Klein <m@mklein.co.at>
2015-01-11 19:32:09 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='gpgpass',
+	version='0.1',
+	description='GPG password manager',
+	url='https://github.com/kleiinnn/gpgpass',
+	author='Markus Klein',
+	author_email='m@mklein.co.at',
+	license='MIT',
+	keywords='gpg gnupg password manager'
+	install_requires=['gnupg']	
+)
