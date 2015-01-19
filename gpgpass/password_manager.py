b2391ea993c0ae4e26f81b9405baef788e0cea0f
Markus Klein <m@mklein.co.at>
2015-01-19 13:31:32 +0100

    do not require a passphrase

--- gpgpass/password_manager.py
+++ gpgpass/password_manager.py
@@ -3,7 +3,7 @@ import os.path
 import json
 
 class PasswordManager:
-    def __init__(self, key_id, passphrase, homedir=os.path.join(os.path.expanduser('~'), '.gnupg'), password_file=os.path.join(os.path.expanduser('~'), '.gpgpass'), operation_mode=1):
+    def __init__(self, key_id, passphrase=None, homedir=os.path.join(os.path.expanduser('~'), '.gnupg'), password_file=os.path.join(os.path.expanduser('~'), '.gpgpass'), operation_mode=1):
         self._gpg = gnupg.GPG(homedir=homedir, binary='gpg')
         self._gpg.encoding = 'utf-8'
         self._operation_mode = operation_mode
@@ -48,7 +48,7 @@ class PasswordManager:
     def save(self):
         fp = open(self.password_file, 'w', encoding='ascii')
         if self._operation_mode == 1:
-            password_file_content = str(self._gpg.encrypt(json.dumps(self.passwords), self.key_id, passphrase=self._passphrase, default_key=self.key_id))
+            password_file_content = str(self._gpg.encrypt(json.dumps(self.passwords), self.key_id, default_key=self.key_id, passphrase=self._passphrase))
         fp.write(password_file_content)
         fp.close()
         return None
